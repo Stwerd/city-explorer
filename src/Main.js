bdae0ad40abdbdedc2d40eb8590562aa45fdea4a
Jackson Gurney <jacko7557@gmail.com>
2022-08-24 12:01:45 -0700

    reached goals

--- src/Main.js
+++ src/Main.js
@@ -11,6 +11,7 @@ class Main extends React.Component {
     this.state = {
       input: '',
       location: [],
+      description: [],
       map: ''
     };
   }
@@ -18,28 +19,37 @@ class Main extends React.Component {
     e.preventDefault();
     console.log(this.state.input);
     try {
-      console.log(`submitted form and called handleSubmitCity ${this.state.input}`);
 
-      const getIt = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.input}&format=json`);
-      //const getForecast = await axios.get(`${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.input}`);
+      const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.input}&format=json`
+      const getIt = await axios.get(url);
+
+      let forecastURL = `${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.input}`;
+      const getForecast = await axios.get(forecastURL);
+
       this.setState({
         location: getIt.data,
+        description: getForecast.data
       });
-      console.log(getIt.data);
-      //console.log(getForecast.data);
     } catch (error) {
       console.log('error', error);
       console.log('error.message: ', error.message);
     }
   };
   render() {
+    let ForecastDisplay = this.state.description.map((r, idx) => {
+      return <>
+        <Card.Text>{r.date} {r.desc}</Card.Text>
+      </>
+    });
+
     let city = this.state.location.map((r, idx) => {
-      return <Card key={idx} style={{ width: '18vw' }}>
+      return <Card key={idx} style={{ width: '33vw' }}>
         <Card.Body>
-          <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${r.lat},${r.lon}&zoom=13`}/>
+          <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${r.lat},${r.lon}&zoom=12`} />
           <Card.Text>Latitude: {r.lat}</Card.Text>
           <Card.Text>Longitude: {r.lon}</Card.Text>
           <Card.Text>Location: {r.display_name}</Card.Text>
+          {ForecastDisplay}
         </Card.Body>
       </Card>;
     });
@@ -52,7 +62,7 @@ class Main extends React.Component {
           <input onChange={(e) => this.setState({ input: e.target.value })}></input>
           <button>Explore!</button>
         </form>
-        <div id='maps'>{city}</div>
+        <div id='maps'>{city[0]}</div>
       </main>
     );
   }
